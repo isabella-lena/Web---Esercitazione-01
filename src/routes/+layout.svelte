25db45c8c9a2a0ca22009a4ba6fb93b1c4a94638
isabella-lena <Isabella.lena@mail.polimi.it>
2026-03-25 17:28:36 +0100

    lezione 25.03

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { isJsxChild } from 'typescript';
+import Header from './Header.svelte';
+	import './layout.css';
+
+	/** @type {{children: import('svelte').Snippet}} */
+	let { children } = $props();
+</script>
+
+
+{@render children()}
+
+<footer>
+	<p>polimi 2026</p>
+</footer>
+
+<style>
+	:global(p) {
+	color: deeppink;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { on } from "svelte/events";
+
+console.log("hello world")
+let count = $state(0)
+let doubled = $derived(count * 2)
+</script>
+<h1>Welcome to SveteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<button onclick={() => count++}>
+clicks:{count}
+</button>
+
+<span></span>
+<style> 
+h1 {
+	color:deepskyblue;
+
+}
+
+
+
+</style>
